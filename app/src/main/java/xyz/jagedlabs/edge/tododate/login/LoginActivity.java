8ab32fcba1c5fd3f07f83b4ed0d0d829ee86afd9
Gustavo Gomes <gustavofrgomes@hotmail.com>
2018-02-21 14:57:08 +0000

    Created login layout with vanilla widgets

--- app/src/main/java/xyz/jagedlabs/edge/tododate/login/LoginActivity.java
+++ app/src/main/java/xyz/jagedlabs/edge/tododate/login/LoginActivity.java
@@ -1,11 +1,20 @@
 package xyz.jagedlabs.edge.tododate.login;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import xyz.jagedlabs.edge.tododate.R;
+
 /**
- * Created by ggomes on 2/21/2018.
+ * By Gustavo Gomes on 2/21/2018.
  */
 
 public class LoginActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
 }
