e8ea110b0f45ee41827abbfaedb20a461e59dcb3
Gustavo Gomes <gustavofrgomes@hotmail.com>
2018-02-21 14:40:45 +0000

    Created the Login Activity and Layout.
    They are still plain files without any meaningful code.

---
+++ app/src/main/java/xyz/jagedlabs/edge/tododate/login/LoginActivity.java
@@ -0,0 +1,11 @@
+package xyz.jagedlabs.edge.tododate.login;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ggomes on 2/21/2018.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+}
